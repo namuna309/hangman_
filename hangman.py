6ca62089444fe50945093e09cbf557c90d4811fe
Nam Yoonah <nyah309@gmail.com>
2023-04-28 15:53:01 +0900

    changed greeting massage

--- hangman.py
+++ hangman.py
@@ -123,7 +123,7 @@ def checkWrongAnswer(missedLetters, secretWord):
             
 def main():
     """Main application entry point."""
-    print('H A N G M A N by namuna')
+    print('H A N G M A N by nam yoon ah')
     missedLetters = ''
     correctLetters = ''
     gameSucceeded = False
